c5873d90c193022c7408d7ce92e553b4995c7e20
Randy Lai <randy.cs.lai@gmail.com>
2014-06-09 00:25:54 -0700

    update test

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,7 @@ import sublime, sublime_plugin
 class HelloWorldCommand(sublime_plugin.TextCommand):
     def run(self,edit):
         view = self.view
-        view.insert(edit, 0, "hello world")
+        view.insert(edit, view.sel()[0].begin(), "hello world")
 
 def foo(x):
     return x+1
--- tests/test.py
+++ tests/test.py
@@ -3,12 +3,7 @@ from unittest import TestCase
 
 version = sublime.version()
 
-if version<'3000':
-    # st2
-   helloworld =  sys.modules["helloworld"]
-else:
-    # st3
-   helloworld =  sys.modules["UnitTesting-example.helloworld"]
+# for testing sublime command
 
 class test_helloworld_command(TestCase):
 
@@ -20,17 +15,34 @@ class test_helloworld_command(TestCase):
             self.view.set_scratch(True)
             self.view.window().run_command("close_file")
 
+    def setText(self, string):
+        self.view.run_command("insert", {"characters": string})
+
+    def getRow(self, row):
+        return self.view.substr(self.view.line(self.view.text_point(row,0)))
+
     # since ST2 doesn't support unittest.skip, we have to do primitive skipping
     if version>='3000':
         def test_hello_world_st3(self):
             self.view.run_command("hello_world")
-            first_row = self.view.substr(self.view.line(0))
+            first_row = self.getRow(0)
             self.assertEqual(first_row,"hello world")
 
     def test_hello_world(self):
+        self.setText("new ")
         self.view.run_command("hello_world")
-        first_row = self.view.substr(self.view.line(0))
-        self.assertEqual(first_row,"hello world")
+        first_row = self.getRow(0)
+        self.assertEqual(first_row,"new hello world")
+
+# for testing internal function
+
+if version<'3000':
+    # st2
+   helloworld =  sys.modules["helloworld"]
+else:
+    # st3
+   helloworld =  sys.modules["UnitTesting-example.helloworld"]
+
 
 class test_internal_functions(TestCase):
 
