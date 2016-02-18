c2f24820e1c3bfe48d09c68dae3af88f40e4d269
Randy Lai <randy.cs.lai@gmail.com>
2016-02-18 16:01:35 -0500

    fix test pep8 voiolation and make sure close_windows_when_empty is False

--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,16 @@
-import sublime, sys
+import sublime
+import sys
 from unittest import TestCase
 
 version = sublime.version()
 
-# for testing sublime command
 
+# for testing sublime command
 class test_helloworld_command(TestCase):
 
     def setUp(self):
         self.view = sublime.active_window().new_file()
+        self.view.settings().set("close_windows_when_empty", False)
 
     def tearDown(self):
         if self.view:
@@ -20,29 +22,29 @@ class test_helloworld_command(TestCase):
         self.view.run_command("insert", {"characters": string})
 
     def getRow(self, row):
-        return self.view.substr(self.view.line(self.view.text_point(row,0)))
+        return self.view.substr(self.view.line(self.view.text_point(row, 0)))
 
     # since ST2 doesn't support unittest.skip, we have to do primitive skipping
-    if version>='3000':
+    if version >= '3000':
         def test_hello_world_st3(self):
             self.view.run_command("hello_world")
             first_row = self.getRow(0)
-            self.assertEqual(first_row,"hello world")
+            self.assertEqual(first_row, "hello world")
 
     def test_hello_world(self):
         self.setText("new ")
         self.view.run_command("hello_world")
         first_row = self.getRow(0)
-        self.assertEqual(first_row,"new hello world")
+        self.assertEqual(first_row, "new hello world")
+
 
 # for testing internal function
-
-if version<'3000':
+if version < '3000':
     # st2
-   helloworld =  sys.modules["helloworld"]
+    helloworld = sys.modules["helloworld"]
 else:
     # st3
-   helloworld =  sys.modules["UnitTesting-example.helloworld"]
+    helloworld = sys.modules["UnitTesting-example.helloworld"]
 
 
 class test_internal_functions(TestCase):
