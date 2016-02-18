17e8d5d257381d4a2d6af4029323cbcac349993b
Randy Lai <randy.cs.lai@gmail.com>
2016-02-18 16:46:06 -0500

    fix pep8 violations

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,17 @@
-import sublime, sys
+import sublime
+import sys
 from unittest import TestCase
 import subprocess
 
 version = sublime.version()
 
-# for testing sublime command
 
+# for testing sublime command
 class test_helloworld_command(TestCase):
 
     def test_hello_world(self):
         p = subprocess.Popen("sleep 2; echo abc", shell=True, stdout=subprocess.PIPE)
         p.wait()
-        out,err = p.communicate()
+        out, err = p.communicate()
         out = out.decode()
         self.assertEqual(out, "abc\n")
