43b67c7bfcc40c4bcfed0d72538b6c18430d6420
Randy Lai <randy.cs.lai@gmail.com>
2016-10-26 17:53:39 -0400

    fix pep8

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,12 @@
 # example
-import sublime, sublime_plugin
+import sublime_plugin
+
 
 class HelloWorldCommand(sublime_plugin.TextCommand):
-    def run(self,edit):
+    def run(self, edit):
         view = self.view
         view.insert(edit, view.sel()[0].begin(), "hello world")
 
+
 def foo(x):
-    return x+1
+    return x + 1
